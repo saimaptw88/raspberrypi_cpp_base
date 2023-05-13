1f02327844e1c0ead194a4063a13ac113ef2bd33
斎藤正大 <saima.ptw88@gmail.com>
2023-05-13 10:02:57 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## OVERVIEW
+
+## COMMAND
+
+- build
+  `g++ main.cc`
+  `g++ -g3 main.cc` <- for debug
+
+- execute
+  `./a.out`
+
+- debug
+  `gdb ./a.out`
---
+++ main.cc
@@ -0,0 +1,15 @@
+// Copyright 2023 @saito
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello world" << std::endl;
+
+  for (int i = 0; i < 10; ++i)
+  {
+    std::cout << "i = " << i << std::endl;
+  }
+
+  std::cout << "Done" << std::endl;
+  return 0;
+}
